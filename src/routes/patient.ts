a53d1e512eac8b3ed79b8642f1e04f77cdd322f3
GauravPoosarla <poosarlagaurav@gmail.com>
2023-04-25 22:36:34 +0530

    fix: route names

--- src/routes/patient.ts
+++ src/routes/patient.ts
@@ -3,10 +3,10 @@ const router = Router()
 import patientController from '../controllers/patient'
 
 router.get('/checkIfPatientExists/:abhaId', patientController.checkIfPatientExists)
-router.post('/create', patientController.createPatient)
-router.get('/patients', patientController.getPatients)
-router.get('/get/:abhaId', patientController.getPatient)
-router.put('/update', patientController.updatePatient)
-router.delete('/delete/:abhaId', patientController.deletePatient)
+router.post('/createPatient', patientController.createPatient)
+router.get('/allPatients', patientController.getPatients)
+router.get('/getPatient/:abhaId', patientController.getPatient)
+router.put('/updatePatient', patientController.updatePatient)
+router.delete('/deletePatient/:abhaId', patientController.deletePatient)
 
 export default router;
