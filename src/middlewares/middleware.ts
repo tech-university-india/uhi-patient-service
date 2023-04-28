82cb3a4a8b3b74d27d3ef9ec951a124bb30d75f0
GauravPoosarla <poosarlagaurav@gmail.com>
2023-04-28 12:54:37 +0530

    fix: return types

--- src/middlewares/middleware.ts
+++ src/middlewares/middleware.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { createPatientValidation as createPatientValidationSchema } from './schemas';
 
-export const createPatientValidation = (req: Request, res: Response, next: NextFunction): Response<any, Record<string, any>> => {
+export const createPatientValidation = (req: Request, res: Response, next: NextFunction) => {
   const { error } = createPatientValidationSchema.validate(req.body);
   if (error) {
     return res.status(400).json(error.message);
