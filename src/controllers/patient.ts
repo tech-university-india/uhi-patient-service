b4a093cea51fbffe1aca09800b78e43e998d2c64
GauravPoosarla <poosarlagaurav@gmail.com>
2023-04-28 11:52:10 +0530

    chore: add tests for controllers

--- src/controllers/patient.ts
+++ src/controllers/patient.ts
@@ -34,7 +34,7 @@ export const createPatient = async (req: Request, res: Response): Promise<void> 
 export const getPatients = async (req: Request, res: Response): Promise<void> => {
   try {
     const patients = await PatientServices.getPatients()
-    res.status(200).json({ data: patients })
+    res.status(200).json({ message: 'Patients fetched successfully', data: patients })
   } catch (error) {
     res.status(500).json(error.message)
   }
@@ -44,7 +44,7 @@ export const getPatient = async (req: Request, res: Response): Promise<void> => 
   try {
     const { abhaId } = req.params
     const patient = await PatientServices.getPatient(abhaId)
-    res.status(200).json({ message: "Patient Exists", data: patient })
+    res.status(200).json({ message: "Patient fetched successfully", data: patient })
   } catch (error) {
     if (error.message === 'Patient does not exist with this Abha-id') { res.status(404).json(error.message) } else {
       res.status(500).json(error.message)
@@ -56,7 +56,7 @@ export const updatePatient = async (req: Request, res: Response): Promise<void> 
   try {
     const { abhaId, name, gender, yearOfBirth, monthOfBirth, dayOfBirth, address, mobile, healthNumber } = req.body
     const patient = await PatientServices.updatePatient({ abhaId, name, gender, yearOfBirth, monthOfBirth, dayOfBirth, address, mobile, healthNumber })
-    res.status(200).json({ message: 'Patient updated', data: patient })
+    res.status(200).json({ message: 'Patient updated successfully', data: patient })
   } catch (error) {
     res.status(500).json(error.message)
   }
@@ -66,7 +66,7 @@ export const deletePatient = async (req: Request, res: Response): Promise<void> 
   try {
     const { abhaId } = req.params
     await PatientServices.deletePatient(abhaId)
-    res.status(200).json({ message: 'Patient deleted' })
+    res.status(200).json({ message: 'Patient deleted successfully' })
   } catch (error) {
     res.status(500).json(error.message)
   }
--- test/controllers/patient.test.ts
+++ test/controllers/patient.test.ts
@@ -1,297 +1,322 @@
-import patientController from '../../src/controllers/patient'
-import patientService from '../../src/services/patient'
+import { Request, Response } from 'express';
+import patientController from '../../src/controllers/patient';
+import patientService from '../../src/services/patient';
+import { PatientInstance } from '../../src/models/patient';
 
 describe('Patient Controller', () => {
   describe('checkIfPatientExists', () => {
     it('should return 200 if there was no error', async () => {
       const req = {
         params: {
-          abhaId: '1234567890'
-        }
-      }
+          abhaId: '1234567890',
+        },
+      } as unknown as Request;
 
       const res = {
         status: jest.fn().mockReturnThis(),
-        json: jest.fn()
-      }
+        json: jest.fn(),
+      } as unknown as Response;
 
       const resolvedValue = {
-        abhaId: '1234567890'
-      }
+        abhaId: '1234567890',
+      } as unknown as PatientInstance
 
-      jest.spyOn(patientService, 'checkIfPatientExists').mockResolvedValue(resolvedValue)
-      await patientController.checkIfPatientExists(req, res)
-      expect(res.status).toHaveBeenCalledWith(200)
-      expect(res.json).toHaveBeenCalled()
-    })
+      jest.spyOn(patientService, 'checkIfPatientExists').mockResolvedValue(resolvedValue);
+      await patientController.checkIfPatientExists(req, res);
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith({
+        message: 'Patient exists', data: {
+          abhaId: resolvedValue.abhaId,
+        }
+      });
+    });
 
     it('should return 404 if the Patient doesnt exist', async () => {
       const req = {
-        body: {
-          abhaId: '1234567890'
-        }
-      }
+        params: {
+          abhaId: '1234567890',
+        },
+      } as unknown as Request;
+
       const res = {
         status: jest.fn().mockReturnThis(),
-        json: jest.fn()
-      }
-      jest.spyOn(patientService, 'checkIfPatientExists').mockImplementation(() => {
-        throw new Error('Patient does not exist')
-      })
-      await patientController.checkIfPatientExists(req, res)
-      expect(res.status).toHaveBeenCalledWith(404)
-      expect(res.json).toHaveBeenCalled()
-    })
+        json: jest.fn(),
+      } as unknown as Response;
+
+      jest.spyOn(patientService, 'checkIfPatientExists').mockRejectedValue(new Error('Patient does not exist'));
+
+      await patientController.checkIfPatientExists(req, res);
+      expect(res.status).toHaveBeenCalledWith(404);
+      expect(res.json).toHaveBeenCalledWith({ exist: false, message: 'Patient does not exist' });
+    });
 
     it('should return 500 if the function throws an error', async () => {
       const req = {
         params: {
-          abhaId: '1234567890'
-        }
-      }
+          abhaId: '1234567890',
+        },
+      } as unknown as Request;
 
       const res = {
         status: jest.fn().mockReturnThis(),
-        json: jest.fn()
-      }
+        json: jest.fn(),
+      } as unknown as Response;
 
       jest.spyOn(patientService, 'checkIfPatientExists').mockImplementation(() => {
-        throw new Error('Error')
-      })
+        throw new Error('Error');
+      });
 
-      await patientController.checkIfPatientExists(req, res)
-      expect(res.status).toHaveBeenCalledWith(500)
-      expect(res.json).toHaveBeenCalled()
-    })
-  })
+      await patientController.checkIfPatientExists(req, res);
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith('Error');
+    });
+  });
 
   describe('createPatient', () => {
-    it('should call patientService.createPatient', async () => {
+    it('should return 200 when patient was created successfully', async () => {
       const req = {
         body: {
-          abhaId: '1234567890'
-        }
-      }
+          abhaId: '1234567890',
+        },
+      } as unknown as Request;
 
       const res = {
         status: jest.fn().mockReturnThis(),
-        json: jest.fn()
-      }
+        json: jest.fn(),
+      } as unknown as Response;
 
       const resolvedValue = {
-        abhaId: '1234567890'
-      }
+        abhaId: '1234567890',
+      } as unknown as PatientInstance;
 
-      jest.spyOn(patientService, 'createPatient').mockResolvedValue(resolvedValue)
-      await patientController.createPatient(req, res)
-      expect(res.status).toHaveBeenCalledWith(200)
-      expect(res.json).toHaveBeenCalled()
-    })
+      jest.spyOn(patientService, 'createPatient').mockResolvedValue(resolvedValue);
+      await patientController.createPatient(req, res);
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith({
+        message: 'Patient created successfully', data: {
+          abhaId: resolvedValue.abhaId,
+        }
+      });
+    });
 
     it('should return 500 if the function throws an error', async () => {
       const req = {
         body: {
-          abhaId: '1234567890'
-        }
-      }
+          abhaId: '1234567890',
+        },
+      } as unknown as Request;
 
       const res = {
         status: jest.fn().mockReturnThis(),
-        json: jest.fn()
-      }
+        json: jest.fn(),
+      } as unknown as Response;
 
       jest.spyOn(patientService, 'createPatient').mockImplementation(() => {
-        throw new Error('Error')
-      })
+        throw new Error('Error');
+      });
 
-      await patientController.createPatient(req, res)
-      expect(res.status).toHaveBeenCalledWith(500)
-      expect(res.json).toHaveBeenCalled()
-    })
-  })
+      await patientController.createPatient(req, res);
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith('Error');
+    });
+  });
 
   describe('getPatients', () => {
-    it('should call patientService.getPatients', async () => {
-      const req = {}
+    it('should return 200 when the patients are fetched successfully', async () => {
+      const req = {} as Request;
 
       const res = {
         status: jest.fn().mockReturnThis(),
-        json: jest.fn()
-      }
+        json: jest.fn(),
+      } as unknown as Response;
 
       const resolvedValue = [{
-        abhaId: '1234567890'
-      }]
+        abhaId: '1234567890',
+      }] as unknown as PatientInstance[];
 
-      jest.spyOn(patientService, 'getPatients').mockResolvedValue(resolvedValue)
-      await patientController.getPatients(req, res)
-      expect(res.status).toHaveBeenCalledWith(200)
-      expect(res.json).toHaveBeenCalled()
-    })
+      jest.spyOn(patientService, 'getPatients').mockResolvedValue(resolvedValue);
+      await patientController.getPatients(req, res);
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith({
+        message: 'Patients fetched successfully', data: resolvedValue
+      });
+    });
 
     it('should return 500 if the function throws an error', async () => {
-      const req = {}
+      const req = {} as Request;
 
       const res = {
         status: jest.fn().mockReturnThis(),
-        json: jest.fn()
-      }
+        json: jest.fn(),
+      } as unknown as Response;
 
       jest.spyOn(patientService, 'getPatients').mockImplementation(() => {
-        throw new Error('Error')
-      })
+        throw new Error('Error');
+      });
 
-      await patientController.getPatients(req, res)
-      expect(res.status).toHaveBeenCalledWith(500)
-      expect(res.json).toHaveBeenCalled()
-    })
-  })
+      await patientController.getPatients(req, res);
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith('Error');
+    });
+  });
 
   describe('getPatient', () => {
-    it('should call patientService.getPatient', async () => {
+    it('should call 200 when the details of the patient are fetched', async () => {
       const req = {
         params: {
-          abhaId: '1234567890'
-        }
-      }
+          abhaId: '1234567890',
+        },
+      } as unknown as Request;
 
       const res = {
         status: jest.fn().mockReturnThis(),
-        json: jest.fn()
-      }
+        json: jest.fn(),
+      } as unknown as Response;
 
       const resolvedValue = {
-        abhaId: '1234567890'
-      }
+        abhaId: '1234567890',
+      } as unknown as PatientInstance;
 
-      jest.spyOn(patientService, 'getPatient').mockResolvedValue(resolvedValue)
-      await patientController.getPatient(req, res)
-      expect(res.status).toHaveBeenCalledWith(200)
-      expect(res.json).toHaveBeenCalled()
-    })
+      jest.spyOn(patientService, 'getPatient').mockResolvedValue(resolvedValue);
+      await patientController.getPatient(req, res);
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith({
+        message: 'Patient fetched successfully', data: {
+          abhaId: resolvedValue.abhaId,
+        }
+      });
+    });
+
     it('should return 404 if the Patient doesnt exist', async () => {
       const req = {
-        body: {
-          abhaId: '1234567890'
-        }
-      }
+        params: {
+          abhaId: '1234567890',
+        },
+      } as unknown as Request;
+
       const res = {
         status: jest.fn().mockReturnThis(),
-        json: jest.fn()
-      }
-      jest.spyOn(patientService, 'getPatient').mockImplementation(() => {
-        throw new Error('Patient does not exist with this Abha-id')
-      })
-      await patientController.getPatient(req, res)
-      expect(res.status).toHaveBeenCalledWith(404)
-      expect(res.json).toHaveBeenCalled()
-    })
+        json: jest.fn(),
+      } as unknown as Response;
+
+      jest.spyOn(patientService, 'getPatient').mockRejectedValue(new Error('Patient does not exist with this Abha-id'));
+
+      await patientController.getPatient(req, res);
+      expect(res.status).toHaveBeenCalledWith(404);
+      expect(res.json).toHaveBeenCalledWith('Patient does not exist with this Abha-id');
+    });
 
     it('should return 500 if the function throws an error', async () => {
       const req = {
         params: {
-          abhaId: '1234567890'
-        }
-      }
+          abhaId: '1234567890',
+        },
+      } as unknown as Request;
 
       const res = {
         status: jest.fn().mockReturnThis(),
-        json: jest.fn()
-      }
+        json: jest.fn(),
+      } as unknown as Response;
 
       jest.spyOn(patientService, 'getPatient').mockImplementation(() => {
-        throw new Error('Error')
-      })
+        throw new Error('Error');
+      });
 
-      await patientController.getPatient(req, res)
-      expect(res.status).toHaveBeenCalledWith(500)
-      expect(res.json).toHaveBeenCalled()
-    })
-  })
+      await patientController.getPatient(req, res);
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith('Error');
+    });
+  });
 
   describe('updatePatient', () => {
-    it('should call patientService.updatePatient', async () => {
+    it('should return 200 when patient details are updated', async () => {
       const req = {
-        params: {
-          abhaId: '1234567890'
-        }
-      }
+        body: {
+          abhaId: '1234567890',
+        },
+      } as unknown as Request;
 
       const res = {
         status: jest.fn().mockReturnThis(),
-        json: jest.fn()
-      }
+        json: jest.fn(),
+      } as unknown as Response;
 
       const resolvedValue = {
-        abhaId: '1234567890'
-      }
+        abhaId: '1234567890',
+      } as unknown as PatientInstance;
 
-      jest.spyOn(patientService, 'updatePatient').mockResolvedValue(resolvedValue)
-      await patientController.updatePatient(req, res)
-      expect(res.status).toHaveBeenCalledWith(200)
-      expect(res.json).toHaveBeenCalled()
-    })
+      jest.spyOn(patientService, 'updatePatient').mockResolvedValue(resolvedValue);
+      await patientController.updatePatient(req, res);
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith({
+        message: 'Patient updated successfully', data: {
+          abhaId: resolvedValue.abhaId,
+        }
+      });
+    });
 
     it('should return 500 if the function throws an error', async () => {
       const req = {
         body: {
-          abhaId: '1234567890'
-        }
-      }
+          abhaId: '1234567890',
+        },
+      } as unknown as Request;
 
       const res = {
         status: jest.fn().mockReturnThis(),
-        json: jest.fn()
-      }
+        json: jest.fn(),
+      } as unknown as Response;
 
       jest.spyOn(patientService, 'updatePatient').mockImplementation(() => {
-        throw new Error('Error')
-      })
+        throw new Error('Error');
+      });
 
-      await patientController.updatePatient(req, res)
-      expect(res.status).toHaveBeenCalledWith(500)
-      expect(res.json).toHaveBeenCalled()
-    })
-  })
+      await patientController.updatePatient(req, res);
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith('Error');
+    });
+  });
 
   describe('deletePatient', () => {
     it('should call patientService.deletePatient', async () => {
       const req = {
-        body: {
-          abhaId: '1234567890'
-        }
-      }
+        params: {
+          abhaId: '1234567890',
+        },
+      } as unknown as Request;
 
       const res = {
         status: jest.fn().mockReturnThis(),
-        json: jest.fn()
-      }
+        json: jest.fn(),
+      } as unknown as Response;
 
-      jest.spyOn(patientService, 'deletePatient').mockResolvedValue(undefined)
-      await patientController.deletePatient(req, res)
-      expect(res.status).toHaveBeenCalledWith(200)
-      expect(res.json).toHaveBeenCalled()
-    })
+      jest.spyOn(patientService, 'deletePatient').mockResolvedValue(1);
+      await patientController.deletePatient(req, res);
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith({
+        message: 'Patient deleted successfully'
+      });
+    });
 
     it('should return 500 if the function throws an error', async () => {
       const req = {
-        body: {
-          abhaId: '1234567890'
-        }
-      }
+        params: {
+          abhaId: '1234567890',
+        },
+      } as unknown as Request;
 
       const res = {
         status: jest.fn().mockReturnThis(),
-        json: jest.fn()
-      }
+        json: jest.fn(),
+      } as unknown as Response;
 
       jest.spyOn(patientService, 'deletePatient').mockImplementation(() => {
-        throw new Error('Error')
-      })
+        throw new Error('Error');
+      });
 
-      await patientController.deletePatient(req, res)
-      expect(res.status).toHaveBeenCalledWith(500)
-      expect(res.json).toHaveBeenCalled()
-    })
-  })
-})
+      await patientController.deletePatient(req, res);
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith('Error');
+    });
+  });
+});
